a0315942b643d01113a1f88df3dbd254a51350a9
fuqu092 <kunal_b@cs.iitr.ac.in>
2025-02-02 17:59:50 +0530

    Readme for Lexer

---
+++ Assignments/Lexer/README.md
@@ -0,0 +1,5 @@
+# Lexer
+To run
+1. `make`
+2. `chmod +x run.sh`
+3. `./run.sh`
--- Assignments/Lexer/run.sh
+++ Assignments/Lexer/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Lexical Analysis for test-1"
 ./lexer < test/test1.c
+echo "Lexical Analysis for test-2"
 ./lexer < test/test2.c
+echo "Lexical Analysis for test-3"
 ./lexer < test/test3.c
+echo "Lexical Analysis for test-4"
 ./lexer < test/test4.c
+echo "Lexical Analysis for test-5"
 ./lexer < test/test5.c
